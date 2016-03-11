586a197e05ff914b63cf4ef7c045d42a3dd0373c
pathmilawk <pathmilawk@gmail.com>
2016-03-11 16:53:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pathmilawk/minijobs/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.example.pathmilawk.minijobs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by PathmilaWK on 3/11/2016.
+ */
+public class HomeFragment extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView=inflater.inflate(R.layout.home_layout, container, false);
+        return myView;
+    }
+}
